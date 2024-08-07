8c671e3aafa03e092fca7e8d0f475bb85f8ace1a
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-08-07 11:27:48 -0400

    add animation and script js

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ function toggleMode() {
     //Substittuir a imagem
     if(html.classList.contains("light")) {
         // Se tiver light mode, adicionar a imagem light
-        img.setAttribute("src", ".assets/avatar.png");
+        img.setAttribute("src", ".assets/avatar-light.png");
+
     }else{
         // Se tiver sem light mode, manter a imagem atual
-        img.setAttribute("src", ".assets/avatar-light.png");
+        img.setAttribute("src", ".assets/avatar.jpg");
     }
 }
