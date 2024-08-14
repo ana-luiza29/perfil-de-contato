12b6b5622f5d78734211a168c6ef0b374cf4bba7
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-08-14 08:52:01 -0400

    finish project

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function toggleMode() {
-    const html = document.documentElement;
-    html.classList.toggle("light");
+  const html = document.documentElement;
+  html.classList.toggle("light");
 
-    //Pegar a tag img 
-    const img = document.querySelector("#profile img");
+  // Pegar a tag img
+  const img = document.querySelector("#profile img");
 
-    //Substittuir a imagem
-    if(html.classList.contains("light")) {
-        // Se tiver light mode, adicionar a imagem light
-        img.setAttribute("src", ".assets/avatar-light.png");
-
-    }else{
-        // Se tiver sem light mode, manter a imagem atual
-        img.setAttribute("src", ".assets/avatar.jpg");
-    }
+  // Substituir a imagem
+  if (html.classList.contains("light")) {
+    // Se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png");
+  } else {
+    // Se tiver sem light mode, manter a imagem atual
+    img.setAttribute("src", "./assets/avatar.png");
+  }
 }
